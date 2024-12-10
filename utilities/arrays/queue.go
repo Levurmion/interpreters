8af4ac41d37ddd5dd080d8b0236e70c81e7611dd
Elbert Timothy <elbert.timothy@touchstoneadvisory.co.uk>
2024-12-10 07:38:22 +0700

    created Queue

--- utilities/arrays/queue.go
+++ utilities/arrays/queue.go
@@ -1,3 +1,27 @@
 package arrays
 
-type Queue[T any] []T
+import "interpreters/utilities/graphs"
+
+type Queue[T any] struct {
+	linkedList *graphs.LinkedList[T]
+}
+
+func NewQueue[T any]() *Queue[T] {
+	return new(Queue[T])
+}
+
+func (q *Queue[T]) Size() int {
+	return q.linkedList.GetLength()
+}
+
+func (q *Queue[T]) Enqueue(item T) {
+	q.linkedList.Unshift(item)
+} 
+
+func (q *Queue[T]) Dequeue() T {
+	return q.linkedList.Pop().Item
+}
+
+func (q *Queue[T]) Top() T {
+	return q.linkedList.GetTail().Item
+}
